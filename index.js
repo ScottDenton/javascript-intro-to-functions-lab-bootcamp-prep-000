fe22cb81c0212ca8a4ff1ca5d62f9c5427a7c94b
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-08 21:28:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function shout(string) {
   return string.toUpperCase();
   
-}
+},
 
 function whisper(string) {
   return string.toLowerCase();
-}
+},
+
 function logShout(string) {
   console.log(string.toLowerCase);
+},
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
 }
+
+('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
