d003b97ba21630de04b11404fc726d9f1cc83f34
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-08 21:41:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase() === string){
   return("I can't hear you!")
   } else if (string.toUpperCase()=== string){return ("YES INDEED !")
-  } else if (string === )
+  } else if (string === "I love you, Grandma.") {
+    
+  }
 }
 
