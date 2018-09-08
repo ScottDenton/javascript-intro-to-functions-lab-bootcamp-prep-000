add8c8ce9a116e3b959155ec11f0768e6c770f1b
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-08 21:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string===lowerCase){
   return("I can\t hear you!")
-  }
+  } else {return ""
 }
 
 describe('sayHiToGrandma(string)', function() {
